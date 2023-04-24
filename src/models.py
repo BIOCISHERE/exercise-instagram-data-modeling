22500b4b4855ef098d5f0e7e60a4e2ed5adcbe79
BIOCISHERE <bioc.is.here.8@gmail.com>
2023-04-24 21:44:50 +0000

    hecho

--- src/models.py
+++ src/models.py
@@ -7,27 +7,44 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = "user"
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    first_name = Column(String(300), nullable=False)
+    last_name = Column(String(300), nullable=False)
+    email = Column(String(300), nullable=False)
+    username = Column(String(300), nullable=False)
+    password = Column(String(300), nullable=False)
+    biography = Column(String(400))
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Followers(Base):
+    __tablename__ = "follower"
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    from_id = Column(Integer, ForeignKey("user.id"))
+    to_id = Column(Integer, ForeignKey("user.id"))
+    user = relationship("User")
 
-    def to_dict(self):
-        return {}
+class Post(Base):
+    __tablename__ = "post"
+    id = Column(Integer, primary_key=True)
+    id_user = Column(Integer, ForeignKey("user.id"))
+    user = relationship("User")
 
+class Comment(Base):
+    __tablename__ = "comment"
+    id = Column(Integer, primary_key=True)
+    id_user = Column(Integer, ForeignKey("user.id"))
+    id_post = Column(Integer, ForeignKey("post.id"))
+    message = Column(String(300), nullable=False)
+    comment = relationship("User")
+    post = relationship("Post")
+
+class Media(Base):
+    __tablename__ = "media"
+    id = Column(Integer, primary_key=True)
+    link_media = Column(String(500), nullable=False)
+    id_post = Column(Integer, ForeignKey("post.id"))
+    post = relationship("Post")
 ## Draw from SQLAlchemy base
 try:
     result = render_er(Base, 'diagram.png')
